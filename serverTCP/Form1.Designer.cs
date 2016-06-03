aae94c0bd70873c0d21f5ffc54c177a891efb058
ibilanchuk <partyzan1996@gmail.com>
2016-06-03 12:57:10 +0300

    11

---
+++ serverTCP/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace serverTCP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.text_Port = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.DisConncet = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.Conncet = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // text_Port
+            // 
+            this.text_Port.Location = new System.Drawing.Point(113, 42);
+            this.text_Port.Name = "text_Port";
+            this.text_Port.Size = new System.Drawing.Size(104, 20);
+            this.text_Port.TabIndex = 21;
+            this.text_Port.Text = "5000";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(80, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 13);
+            this.label2.TabIndex = 20;
+            this.label2.Text = "Port";
+            // 
+            // DisConncet
+            // 
+            this.DisConncet.Enabled = false;
+            this.DisConncet.Location = new System.Drawing.Point(338, 40);
+            this.DisConncet.Name = "DisConncet";
+            this.DisConncet.Size = new System.Drawing.Size(76, 23);
+            this.DisConncet.TabIndex = 19;
+            this.DisConncet.Text = "Disconnect";
+            this.DisConncet.UseVisualStyleBackColor = true;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(78, 68);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(370, 160);
+            this.listBox1.TabIndex = 18;
+            // 
+            // Conncet
+            // 
+            this.Conncet.Location = new System.Drawing.Point(223, 39);
+            this.Conncet.Name = "Conncet";
+            this.Conncet.Size = new System.Drawing.Size(109, 23);
+            this.Conncet.TabIndex = 17;
+            this.Conncet.Text = "Start The Server";
+            this.Conncet.UseVisualStyleBackColor = true;
+            this.Conncet.Click += new System.EventHandler(this.Conncet_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(527, 267);
+            this.Controls.Add(this.text_Port);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.DisConncet);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.Conncet);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox text_Port;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button DisConncet;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button Conncet;
+    }
+}
+
---
+++ serverTCP/SocketCoderBinaryServer.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using System.Net;
+using System.Net.Sockets;
+
+namespace serverTCP
+{
+    class SocketCoderBinaryServer
+    {
+        static ArrayList ClientsList = new ArrayList();
+        static Socket Listener_Socket;
+        static SocketCoderClient Newclient;
+        // (1) Establish The Server
+        public static IPAddress GetIpAddress(AddressFamily type)
+        {
+            var hostName = Dns.GetHostName();
+
+            foreach (var ip in Dns.GetHostAddresses(hostName))
+            {
+                if (ip.AddressFamily == type && !ip.IsIPv6LinkLocal && !ip.IsIPv6SiteLocal && !ip.IsIPv6Multicast)
+                    return ip;
+            }
+
+            return null;
+        }
+        public static string Start_Video_Server(int Port)
+        {
+            try
+            {
+                IPAddress AddressAr = IPAddress.Any;
+             
+               /* try
+                {
+                    System.Net.NetworkInformation.IPStatus status = System.Net.NetworkInformation.IPStatus.Unknown;
+                    try
+                    {
+                        status = new System.Net.NetworkInformation.Ping().Send("google.com.ua").Status;
+                    }
+                    catch {
+
+                    }
+                    if (status != System.Net.NetworkInformation.IPStatus.Success)
+                    {
+                        string iip = new WebClient().DownloadString("http://icanhazip.com/").Trim();
+                        IPAddress p = IPAddress.Parse(iip);
+                        AddressAr = p;
+                    }
+                    else
+                    {
+                        IPAddress AddressAr1 = GetIpAddress(AddressFamily.InterNetwork);
+                        AddressAr = AddressAr1;
+                    }
+
+
+                }
+                catch (Exception) { }*/
+
+                if (AddressAr== null)
+                {
+                    return "Unable to get local address ... Error";
+                }
+
+                Listener_Socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+                Listener_Socket.Bind(new IPEndPoint(AddressAr, Port));
+                Listener_Socket.Listen(10);
+
+                Listener_Socket.BeginAccept(new AsyncCallback(EndAccept), Listener_Socket);
+
+                return ("Listening On Port " + Port + "... OK");
+
+            }
+            catch (Exception ex) { return ex.Message; }
+        }
+
+        // (2) Accept Clients Conncetion
+        private static void EndAccept(IAsyncResult ar)
+        {
+            try
+            {
+                Listener_Socket = (Socket)ar.AsyncState;
+                AddClient(Listener_Socket.EndAccept(ar));
+                Listener_Socket.BeginAccept(new AsyncCallback(EndAccept), Listener_Socket);
+            }
+            catch (Exception) { }
+        }
+
+        // (3) Create a Socket for Each Client and add his Socket to The ArrayList 
+        private static void AddClient(Socket sockClient)
+        {
+            Newclient = new SocketCoderClient(sockClient);
+            ClientsList.Add(Newclient);
+
+            // client.Sock.RemoteEndPoint - " has joined the room"
+            Newclient.SetupRecieveCallback();
+        }
+
+        // (4) Send The Recieved Data to All Clients in The Room
+        private static void OnRecievedData(IAsyncResult ar)
+        {
+            SocketCoderClient client = (SocketCoderClient)ar.AsyncState;
+            byte[] aryRet = client.GetRecievedData(ar);
+
+            if (aryRet.Length < 1)
+            {
+                // client.Sock.RemoteEndPoint - "has left the room"
+                client.ReadOnlySocket.Close();
+                ClientsList.Remove(client);
+                return;
+            }
+            foreach (SocketCoderClient clientSend in ClientsList)
+            {
+                if (client != clientSend)
+                    try
+                    {
+                        clientSend.ReadOnlySocket.Send(aryRet);
+                    }
+                    catch
+                    {
+                        clientSend.ReadOnlySocket.Close();
+                        ClientsList.Remove(client);
+                        return;
+                    }
+            }
+            client.SetupRecieveCallback();
+        }
+
+        public static string ShutDown()
+        {
+            try
+            {
+                Listener_Socket.Close();
+                GC.Collect();
+                GC.WaitForPendingFinalizers();
+                return ("Shutdown ... OK");
+            }
+            catch (Exception ex) { return (ex.Message); }
+
+        }
+
+        internal class SocketCoderClient
+        {
+            // To create a new socket for each client 
+
+            private Socket New_Socket;
+            private byte[] buffer = null;
+
+            public SocketCoderClient(Socket PassedSock)
+            {
+                New_Socket = PassedSock;
+            }
+
+            public Socket ReadOnlySocket
+            {
+                get { return New_Socket; }
+            }
+
+            public void SetupRecieveCallback()
+            {
+                try
+                {
+                    buffer = new byte[2205];
+                    AsyncCallback recieveData = new AsyncCallback(SocketCoderBinaryServer.OnRecievedData);
+                    New_Socket.BeginReceive(buffer, 0, buffer.Length, SocketFlags.None, recieveData, this);
+                }
+                catch (Exception)
+                {
+                }
+            }
+            public byte[] GetRecievedData(IAsyncResult ar)
+            {
+                int nBytesRec = 0;
+                try
+                {
+                    nBytesRec = New_Socket.EndReceive(ar);
+                }
+                catch { }
+                byte[] byReturn = new byte[nBytesRec];
+                Array.Copy(buffer, byReturn, nBytesRec);
+                return byReturn;
+            }
+        }
+
+    }
+}
